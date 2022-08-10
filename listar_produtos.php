3f3e0222312818b160d6bf9ba6ec5138c744e1c9
MateusMAART <101677318+MatteusMart@users.noreply.github.com>
2022-08-09 22:25:21 -0300

    adicionando listagem php

---
+++ listar_produtos.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Lista de Produtos</title>
+</head>
+<body>
+    <div id="container">
+    <h3>Listar produtos</h3>
+        <figure>
+            <img src="img/viagem-faltando.png" alt="">
+            <figcaption>
+                <h3>nome</h3>
+                <h3>categoria</h3>
+                <h3>fabricante</h3>
+                <h3>R$</h3>
+                <small></small>
+            </figcaption>
+        </figure>
+    </div>
+    
+</body>
+</html>
